4775c6c2a71a5d8ffba57c61704bdd0113897a72
JLandion <jl.andiontapiz@protonmail.com>
2026-03-15 00:07:13 +0000

    comment weapon_property sub-importation

--- odoomancy_data_import/models/odoomancy_importer_mixin.py
+++ odoomancy_data_import/models/odoomancy_importer_mixin.py
@@ -53,6 +53,7 @@ class OdoomancyImporterMixin(models.AbstractModel):
         reverse_selection = {v: k for k, v in selection.items()}
 
         return reverse_selection.get(label)
+
     # ----- GENERIC LOGIC -----
 
     def action_import(self):
